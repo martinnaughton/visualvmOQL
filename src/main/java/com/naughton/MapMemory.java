fc5e7c728a6134e3ec8d2fec18183763571b5b41
martinnaughton <martin.oneachtain@gmail.com>
2016-09-22 21:06:20 +0100

    Added more OQL queries to show how it works.They are referrer queries so you can find the was objects are referencing the hashmaps that are causing your memory issue. Also updated the test programme that is used to create a heap dump with an array list and adding a few hash maps to it. this then can test the referrer queries.

--- src/main/java/com/naughton/MapMemory.java
+++ src/main/java/com/naughton/MapMemory.java
@@ -1,12 +1,15 @@
 package com.naughton;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MapMemory {
 	static short mapSize = 10000;
 
 	public static void main(String[] args) {
+		List<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
 		Map<Integer, String> stringMap = new HashMap<Integer, String>();
 		System.out.println("map size:"+stringMap.size());
 		for (int index = 0; index < mapSize; index+=2) {
@@ -15,8 +18,28 @@ public class MapMemory {
 			stringMap.put(index+2, new String("Whale:" + index+2));
 		}
 		System.out.println("map size:"+stringMap.size());
+		storeAMapInArrayList(list);
 		while (true) {
 		}
+	}
+
+	private static void storeAMapInArrayList(List<HashMap<String, String>> list) {
+		HashMap<String, String> stringMap = new HashMap<String, String>();
+		stringMap.put("DogKey", "DogValue");
+		
+		HashMap<String, String> stringMap2 = new HashMap<String, String>();
+		stringMap2.put("DogKey2", "DogValue");
+		
+		HashMap<String, String> stringMap3 = new HashMap<String, String>();
+		stringMap3.put("DogKey3", "DogValue");
+		
+		HashMap<String, String> stringMap4 = new HashMap<String, String>();
+		stringMap4.put("DogKey4", "DogValue");
+		
+		list.add(stringMap);
+		list.add(stringMap2);
+		list.add(stringMap3);
+		list.add(stringMap4);
 	}
 
 }
